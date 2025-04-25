de6bc032fdaecce890190f84f2f9ef1325e89117
GitGort <ger2567@g.rit.edu>
2025-04-25 09:31:21 -0400

    some pseudocode

--- VigenereCipher.cpp
+++ VigenereCipher.cpp
@@ -10,6 +10,13 @@ VigenereCipher::VigenereCipher(string k) : key(k), Cipher("Vigenere") {;}
  */
 bool VigenereCipher::encode(string msg) {
   /*TODO: write your code*/
+  string longkey;
+  //first use for loop to extend key by storing loop in longkey (use modulus)
+  //remember that a key character is not given for spaces!!!
+
+  //for the length of the msg string
+  //encoded.insert(i, 1, (encode formula output))
+  //remember to check for invalid characters!!!
 } 
 
 /*
@@ -17,4 +24,10 @@ bool VigenereCipher::encode(string msg) {
  */
 bool VigenereCipher::decode(string msg) {
   /*TODO: write your code*/
+  //first use for loop to extend key by storing loop in longkey (use modulus)
+  //remember that a key character is not given for spaces!!!
+
+  //for the length of the msg string
+  //encoded.insert(i, 1, (decode formula output))
+  //remember to check for invalid characters!!!
 }
