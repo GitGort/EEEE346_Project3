bab47f49fa4ea4187da59718f3008813ccf55938
GitGort <ger2567@g.rit.edu>
2025-04-27 12:02:41 -0400

    Long Key generation complete

--- VigenereCipher.cpp
+++ VigenereCipher.cpp
@@ -13,10 +13,37 @@ bool VigenereCipher::encode(string msg) {
   string longkey;
   //first use for loop to extend key by storing loop in longkey (use modulus)
   //remember that a key character is not given for spaces!!!
+  int keyIndex = 0;
+  for (int i = 0; i < msg.size(); i++)
+  {
+    if (isValidCharacter(key.at(keyIndex)))
+    {
+      if (msg.at(i) == ' ') //if space in message
+      {
+        longkey.insert(i, 1, ' '); //insert space in longkey
+      }
+      else
+      {
+        longkey.insert(i, 1, key.at(keyIndex));
+        keyIndex = (keyIndex + 1) % (key.size());
+        //           ^increment index   ^reset index if end of key is reached
+      }
+    }
+    else {return false;} //throw error if key character is invalid
+  }
+
+  //output longkey for testing purposes, comment out later
+  /*
+  test successful, removing test output
+  for (int i = 0; i < msg.size(); i++) {encoded.insert(i, 1, longkey.at(i));}
+  */
 
   //for the length of the msg string
   //encoded.insert(i, 1, (encode formula output))
   //remember to check for invalid characters!!!
+
+  //encode successful!
+  return true;
 } 
 
 /*
@@ -24,9 +51,28 @@ bool VigenereCipher::encode(string msg) {
  */
 bool VigenereCipher::decode(string msg) {
   /*TODO: write your code*/
+  string longkey;
   //first use for loop to extend key by storing loop in longkey (use modulus)
   //remember that a key character is not given for spaces!!!
-
+  int keyIndex = 0;
+  for (int i = 0; i < msg.size(); i++)
+  {
+    if (isValidCharacter(key.at(keyIndex)))
+    {
+      if (msg.at(i) == ' ') //if space in message
+      {
+        longkey.insert(i, 1, ' '); //insert space in longkey
+      }
+      else
+      {
+        longkey.insert(i, 1, key.at(keyIndex));
+        keyIndex = (keyIndex + 1) % (key.size());
+        //           ^increment index   ^reset index if end of key is reached
+      }
+    }
+    else {return false;} //throw error if key character is invalid
+  }
+  
   //for the length of the msg string
   //encoded.insert(i, 1, (decode formula output))
   //remember to check for invalid characters!!!
